d0daaf7bb4dc20aebed7ce02339a663c18c74728
111geon <yangdaz3@gmail.com>
2021-05-30 16:42:23 +0900

    MODIFY: 카카오 회원가입시 User 테이블 관리를 위한 모델 수정 및 migration 파일 생성

--- users/models.py
+++ users/models.py
@@ -1,20 +1,25 @@
 from django.db        import models
 
 class User(models.Model):
-    name                 = models.CharField(max_length=45, null=True)
-    profile_url          = models.URLField(max_length=2000, default='', null=True)
-    password             = models.CharField(max_length=1000, null=True)
+    name                 = models.CharField(max_length=45)
+    email                = models.CharField(max_length=1000)
+    password             = models.CharField(max_length=1000)
+    kakao_id             = models.IntegerField(null=True)
     phone_number         = models.CharField(max_length=100, null=True)
-    marketing_agree      = models.IntegerField(null=True)
+    profile_url          = models.URLField(max_length=2000, null=True)
     refund_account       = models.CharField(max_length=1000, null=True)
-    kakao_account        = models.CharField(max_length=1000, null=True)
-    point                = models.IntegerField(default=0, null=True)
-    kakao                = models.IntegerField(null=True)
-    kakao_email          = models.CharField(max_length=1000, null=True)
-    kakao_nickname       = models.CharField(max_length=200, null=True)
+    marketing_agreement  = models.IntegerField(null=True)
+    point                = models.IntegerField(default=0)
     coupon               = models.ManyToManyField("Coupon", through="UserCoupon", null=True)
+    refresh_token        = models.CharField(max_length=1000, null=True)
     class Meta:
         db_table = "users"
+
+class KakaoToken(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=2000)
+    class Meta:
+        db_table = "kakao_tokens"
 
 class UserCoupon(models.Model):
     user                 = models.ForeignKey(User, on_delete=models.CASCADE)
